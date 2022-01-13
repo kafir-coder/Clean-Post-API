3d65dd18bf5b1be594b606e4189c33230f1730e2
Caio Tony <caiotony16@gmail.com>
2022-01-13 11:06:35 +0100

    refactor: inject listPostUsecase in Controller

--- src/presentation/controllers/list-posts.spec.ts
+++ src/presentation/controllers/list-posts.spec.ts
@@ -1,19 +1,47 @@
+/* eslint-disable require-jsdoc */
 /* eslint-disable max-len */
 import {EmptyParamError, MissingParamError} from '../errors/';
 import {badRequest} from '../helpers/http-helpers';
 import {Controller} from '../protocols/controller';
+import {ListPostUsecase, PaginationParams} from '@domain/usecase/list-posts';
+import {Post} from '@domain/models/post';
 import {ListPost} from './list-posts';
-const makeSut = (): Controller => new ListPost();
+
+
+class MockListPost implements ListPostUsecase {
+  async listPost(paginate: PaginationParams): Promise<Post[]> {
+    return [
+      {
+        content: 'dfasdaf',
+        images: ['asdfaf'],
+        name: 'hi',
+        tags: ['saf'],
+      },
+    ];
+  }
+}
+type sutTypes = {
+  sut: Controller<any>
+  listPosts: ListPostUsecase
+}
+const makeSut = (): sutTypes => {
+  const listPosts: ListPostUsecase = new MockListPost();
+  // @ts-ignore
+  const sut = new ListPost(listPosts);
+
+  return {sut, listPosts};
+};
+
 describe('list post controller ', () => {
   it('list-post controller should receive request object', async () => {
-    const sut = makeSut();
+    const {sut} = makeSut();
     // @ts-ignore
     const result = await sut.handle();
     expect(result).toEqual(badRequest(new MissingParamError('request')));
   });
 
   it('list-post controller should return MissingParamError if request doesnt has a query object', async () => {
-    const sut = makeSut();
+    const {sut} = makeSut();
 
     const request = {};
 
@@ -23,11 +51,16 @@ describe('list post controller ', () => {
 
   // eslint-disable-next-line max-len
   it('list-post controller should return EmptyParamError if request.query is empty', async () => {
-    const sut = makeSut();
+    const {sut} = makeSut();
     const request = {
       query: {},
     };
     const result = await sut.handle(request);
     expect(result).toEqual(badRequest(new EmptyParamError('request.query')));
+  });
+
+  it('list-post controller should receive a ListPost.list injection', async () => {
+
+
   });
 });
